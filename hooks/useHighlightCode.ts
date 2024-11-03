2f5518cc91ce09aa8f5247d59f249c3d81de6014
devvAyman <aymanechakar03@gmail.com>
2024-11-04 00:03:45 +0100

    Most of the work

--- hooks/useHighlightCode.ts
+++ hooks/useHighlightCode.ts
@@ -4,34 +4,34 @@ import { useCallback, useEffect, useState } from "react";
 import { codeToHtml } from "shiki";
 
 export const useHighlightCode = () => {
-  const { language, theme, code, formatting } = useControlsStore();
-  const [highlightedHtml, setHighlightedHtml] = useState("");
+    const { language, theme, code, formatting } = useControlsStore();
+    const [highlightedHtml, setHighlightedHtml] = useState("");
 
-  const highlightCode = useCallback(
-    async (codeToHighlight: string) => {
-      if (!codeToHighlight) {
-        setHighlightedHtml("");
-        return;
-      }
+    const highlightCode = useCallback(
+        async (codeToHighlight: string) => {
+            if (!codeToHighlight) {
+                setHighlightedHtml("");
+                return
+            }
 
-      try {
-        const html = await codeToHtml(codeToHighlight, {
-          lang: language.value,
-          theme: allThemes.find((t) => t.displayName === theme)!,
-        });
-        setHighlightedHtml(html);
-      } catch (error) {
-        console.error("Error highlighting code: ", error);
-      }
-    },
-    [language, theme],
-  );
+            try {
+                const html = await codeToHtml(codeToHighlight, {
+                    lang: language.value,
+                    theme: allThemes.find((t) => t.displayName === theme)!,
+                });
+                setHighlightedHtml(html);
+            } catch (error) {
+                console.error("Error highlighting code: ", error);
+            }
+        },
+        [language, theme],
+    );
 
-  useEffect(() => {
-    if (code) {
-      highlightCode(code);
-    }
-  }, [code, theme, highlightCode, formatting]);
+    useEffect(() => {
+        if (code) {
+            highlightCode(code);
+        }
+    }, [code, theme, highlightCode, formatting]);
 
-  return { highlightCode, highlightedHtml };
+    return { highlightCode, highlightedHtml };
 };
